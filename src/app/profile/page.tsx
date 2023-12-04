8ee4f27d85e1a21e4f83ec0c221356b6e124cf78
gxyzachira <achira.wangsunthorn.dev@gmail.com>
2023-12-04 20:30:35 +0700

    create qr page

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import Box from '@/components/box/Box'
+import styles from './profile.module.css'
+
+export default function ProfilePage() {
+  
+
+  return (
+    <div className={styles.container}>Hello Profile Page <Box /></div>
+  )
+}
